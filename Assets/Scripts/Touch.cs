a482acf70aa540c4094e440c343158bfe3bd83aa
hamch <hamada553057@gmail.com>
2018-09-21 12:40:54 +0900

    更新

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+    private GameObject title;
+    
+	// Use this for initialization
+	void Start () {
+        title = GameObject.Find("tit");
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
