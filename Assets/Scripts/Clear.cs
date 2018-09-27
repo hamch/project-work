b0ab3109e479143c5ae87f991c66462fec05ee23
hamch <hamada553057@gmail.com>
2018-09-27 09:10:44 +0900

    26日

--- Assets/Scripts/Clear.cs
+++ Assets/Scripts/Clear.cs
@@ -5,15 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Clear : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Goal")
