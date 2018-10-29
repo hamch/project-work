d93305cc1e2e94940b3544870983049e2cdef679
hamch <hamada553057@gmail.com>
2018-10-29 12:40:55 +0900

    no message

---
+++ Assets/Scripts/Down.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Down : MonoBehaviour {
+    public float totalTime;
+    public int seconds;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        Debug.Log("totalTime"); 
+	}
+    
+}
